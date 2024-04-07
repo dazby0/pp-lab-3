8425b4d7f6b023b22dbc2539d7bf23130ded383b
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-04-07 14:28:50 +0200

    zad1 done

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(3.5, 2.0);
+
+        System.out.println("Coordinates of a point:");
+        System.out.println("x = " + point.getX());
+        System.out.println("y = " + point.getY());
+    }
+}
