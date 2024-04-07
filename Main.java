8dddcef5278e23af31a859cc61952a5bdab41076
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-04-07 14:31:22 +0200

    zad2 done

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
+import geometry.Circle;
+import geometry.Point;
+
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point(3.5, 2.0);
+        Point center = new Point(3.0, 4.0);
 
-        System.out.println("Coordinates of a point:");
-        System.out.println("x = " + point.getX());
-        System.out.println("y = " + point.getY());
+        Circle circle = new Circle(center, 5.0);
+
+        System.out.println("Circle Properties:");
+        System.out.println("Center of the circle: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Radius of the circle: " + circle.getRadius());
+        System.out.println("Perimeter of the circle: " + circle.calculatePerimeter());
+        System.out.println("Area of the circle: " + circle.getArea());
     }
 }
