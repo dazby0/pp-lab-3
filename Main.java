8164cf79246c9b5444e2178826838f10856d5784
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-04-07 14:33:05 +0200

    zad3 done

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
-import geometry.Circle;
+import geometry.ColoredCircle;
 import geometry.Point;
 
 public class Main {
     public static void main(String[] args) {
         Point center = new Point(3.0, 4.0);
 
-        Circle circle = new Circle(center, 5.0);
+        ColoredCircle coloredCircle = new ColoredCircle(center, 5.0, "blue");
 
-        System.out.println("Circle Properties:");
-        System.out.println("Center of the circle: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
-        System.out.println("Radius of the circle: " + circle.getRadius());
-        System.out.println("Perimeter of the circle: " + circle.calculatePerimeter());
-        System.out.println("Area of the circle: " + circle.getArea());
+        System.out.println("Properties of the Colored Circle:");
+        System.out.println("Center of the circle: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
+        System.out.println("Radius of the circle: " + coloredCircle.getRadius());
+        System.out.println("Color of the circle: " + coloredCircle.getColor());
+        System.out.println("Perimeter of the circle: " + coloredCircle.calculatePerimeter());
+        System.out.println("Area of the circle: " + coloredCircle.getArea());
     }
 }
