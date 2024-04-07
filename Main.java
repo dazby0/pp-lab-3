dd891dab2cb82c76472aa870864f454d0e4796cc
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-04-07 14:34:03 +0200

    zad4 done

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
 import geometry.ColoredCircle;
 import geometry.Point;
+import geometry.Circle;
 
 public class Main {
     public static void main(String[] args) {
-        Point center = new Point(3.0, 4.0);
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(new Point(0, 0), 3);
+        circles[1] = new ColoredCircle(new Point(1, 1), 4, "red");
+        circles[2] = new ColoredCircle(new Point(2, 2), 5, "blue");
 
-        ColoredCircle coloredCircle = new ColoredCircle(center, 5.0, "blue");
+        for (Circle circle : circles) {
+            System.out.println("Area: " + circle.getArea());
 
-        System.out.println("Properties of the Colored Circle:");
-        System.out.println("Center of the circle: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
-        System.out.println("Radius of the circle: " + coloredCircle.getRadius());
-        System.out.println("Color of the circle: " + coloredCircle.getColor());
-        System.out.println("Perimeter of the circle: " + coloredCircle.calculatePerimeter());
-        System.out.println("Area of the circle: " + coloredCircle.getArea());
+            if (circle instanceof ColoredCircle) {
+                ColoredCircle coloredCircle = (ColoredCircle) circle;
+                System.out.println("Color: " + coloredCircle.getColor());
+            }
+        }
     }
 }
