37deb0706a2064c6b9bdb063c142abb370dcaabd
Wojciech <kowalczyk.wojtek168@gmail.com>
2024-04-07 14:35:13 +0200

    zad5 done

--- Main.java
+++ Main.java
@@ -1,21 +1,13 @@
-import geometry.ColoredCircle;
-import geometry.Point;
-import geometry.Circle;
+import geometry.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-        Circle[] circles = new Circle[3];
-        circles[0] = new Circle(new Point(0, 0), 3);
-        circles[1] = new ColoredCircle(new Point(1, 1), 4, "red");
-        circles[2] = new ColoredCircle(new Point(2, 2), 5, "blue");
+        Rectangle rectangle = new Rectangle(4.0, 3.0);
 
-        for (Circle circle : circles) {
-            System.out.println("Area: " + circle.getArea());
-
-            if (circle instanceof ColoredCircle) {
-                ColoredCircle coloredCircle = (ColoredCircle) circle;
-                System.out.println("Color: " + coloredCircle.getColor());
-            }
-        }
+        System.out.println("Properties of the Rectangle:");
+        System.out.println("Length: " + rectangle.getLength());
+        System.out.println("Width: " + rectangle.getWidth());
+        System.out.println("Area: " + rectangle.calculateArea());
+        System.out.println("Perimeter: " + rectangle.calculatePerimeter());
     }
 }
